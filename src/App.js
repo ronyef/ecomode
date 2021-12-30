43138a2771dc470116d7bfc3ad20bcd3f5980202
ronyef <rony.efendy@gmail.com>
2021-12-30 17:54:27 +0700

    challenge added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import {
   Row,
   Col,
   Card,
-  Button
+  Button,
+  Stack,
+  Image
 } from "react-bootstrap";
 
 function App() {
@@ -16,44 +18,62 @@ function App() {
       <TopHeader />
       <Container className="pt-5 pb-4">
         <Row>
-          <Col>
+          <Col sm={12} md={6}>
+            <Stack direction="horizontal">
+              <Image src="./images/ecoMode.png" style={{width: '100px'}} />
+              <Image src="./images/strongMode.png" style={{width: '80px'}}/>
+            </Stack>
             <h2>Power & Economy Mode</h2>
             <p>
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour, or randomised words which don't look even
-              slightly believable. If you are going to use a passage of Lorem
-              Ipsum, you need to be sure there isn't anything embarrassing
-              hidden in the middle of text. All the Lorem Ipsum generators on
-              the Internet tend to repeat predefined chunks as necessary, making
-              this the first true generator on the Internet. It uses a
-              dictionary of over 200 Latin words, combined with a handful of
-              model sentence structures, to generate Lorem Ipsum which looks
-              reasonable. The generated Lorem Ipsum is therefore always free
-              from repetition, injected humour, or non-characteristic words etc.
+              Pemilihan mode yang tepat saat pengoperasian adalah sangat penting untuk produktifitas.
+              Operator memiliki kecenderungan untuk mengaktifkan powerful mode saat bekerja. Karena dengan mode ini,
+              dia akan mendapatkan output power yang maksimal. Namun penggunaan power mode yang berlebihan akan mengakibatkan
+              pemborosan fuel dan over power yang berpengaruh pada komponen-komponen mesin.
+            </p>
+            <p>
+              Begitu pula sebaliknya. Penggunaan econo mode yang berkepanjangan juga dapat menurunkan produktifitas karena kurangnya
+              power yang berakibat terhadap kecepatan unit, kurangnya power saat muatan dan tanjakan, juga dapat mempengaruhi lifetime komponen-komponen
+              mesin karena terjadi pemaksaan untuk mengejar performa.
             </p>
           </Col>
-          <Col>
+          <Col sm={12} md={6} className="mt-4 mt-sm-0 ps-sm-5">
             <Card>
               <Card.Img
                 variant="top"
-                src="https://cdn.glitch.me/873f6cf0-8698-4dc6-95c1-7cc6037da033/operator.png?v=1640835554239"
+                src="./images/operator.png"
               />
               <Card.Body>
                 <Card.Title>Operator Anda Terlalu Sibuk</Card.Title>
-                <Card.Text>
+                <Card.Text >
                   Terlalu banyak hal yang dipikirkan dan dilakukan oleh operator
                   saat mengoperasikan haul truck. Dari mulai memikirkan rute
                   jalan, tanjakan, turunan, komunikasi radio, loading, dumping,
                   penggunaan retarder, service brake, dan yang paling penting
                   adalah memikirkan safety saat beroperasi.
                 </Card.Text>
-                <Button variant="dark">Go Pelajari</Button>
+                <span className='fs-5 fw-bold'>Hal ini mengakibatkan kelalaian dalam pemilihan mode operasi yang tepat.</span>
               </Card.Body>
             </Card>
           </Col>
         </Row>
       </Container>
+      <div className='my-4' style={{backgroundImage: "url('./images/miningTruck1.png')", backgroundSize: 'cover',}}>
+        <Container className="py-4">
+          <Row>
+            <Col md={{ span: 6, offset: 6 }}>
+              <h2 className="text-dark">Pemilihan mode yang tepat:</h2>
+              <ul className="text-dark">
+                <li>Eco Mode - Saat muatan kosong.</li>
+                <li>Power Mode - Saat muatan kosong tanjakan.</li>
+                <li>Power Mode - Saat bermuatan.</li>
+                <li>Eco Mode - Bermuatan di turunan.</li>
+                <li>Eco Mode - Bermuatan saat beban transmisi ringan.</li>
+              </ul>
+              <p className="text-dark">Begitu banyak kondisi sehingga operator mengaktifkan Power Mode terus menerus.</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
