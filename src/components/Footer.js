b7d8922aba57a61593ba1db1e93114023d8c3aa8
ronyef <rony.efendy@gmail.com>
2022-01-01 16:35:30 +0700

    footer didandani

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,28 +10,34 @@ export default function Footer() {
         <Col>
           <Image src="images/mapic2.png" style={{ height: "120px" }} />
         </Col>
+
         <Col>
-          <div className="vr" style={{ height: "100px" }}></div>
-        </Col>
-        <Col>
-          <Stack direction="vertical fs-6">
-            <span>
-              <AiOutlineMail className="me-2" /> sales@mekatronik.cc
-            </span>
-            <span>
-              <AiOutlinePhone className="me-2" /> +62 821 1605 5798
-            </span>
-            <span>
-              <BsWhatsapp className="me-2" /> 0812 20 0812 76
-            </span>
+          <Stack direction="horizontal">
+            <div className="vr" style={{ height: "100px" }}></div>
+            <div className="ms-2">
+            <Stack direction="vertical fs-6">
+              <span>
+                <AiOutlineMail className="me-2" /> sales@mekatronik.cc
+              </span>
+              <span>
+                <AiOutlinePhone className="me-2" /> +62 821 1605 5798
+              </span>
+              <span>
+                <BsWhatsapp className="me-2" /> 0812 20 0812 76
+              </span>
+            </Stack>
+            </div>
           </Stack>
         </Col>
         <Col>
-          <div className="vr" style={{ height: "100px" }}></div>
-        </Col>
-        <Col>
-          <h2>Get Quotation!</h2>
-          And start your improvement project
+          <Stack direction="horizontal">
+            <div className="vr" style={{ height: "100px" }}></div>
+            <Image src="./images/lampu.png" style={{ height: "100px" }} />
+            <div className="ms-2">
+              <h2>Get Quotation!</h2>
+              And start your improvement project
+            </div>
+          </Stack>
         </Col>
       </Row>
     </div>
